65bf156438a1cad5f0d028553e35f173a961aba5
RafsanRifat <rafsan.rifat255@gmail.com>
2022-03-24 12:59:27 +0600

    deleted old data and entering new

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, null=True)
+    # nametty = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
@@ -55,6 +56,7 @@ class User(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    # nametty = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     tc = models.BooleanField()
     is_active = models.BooleanField(default=True)
