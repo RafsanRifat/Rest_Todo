b7fe55d960c7a1283421bce9b8aa67265daef103
RafsanRifat <rafsan.rifat255@gmail.com>
2022-03-24 17:38:28 +0600

    some chabges

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+from rest_framework.views import APIView
 
 
 # Create your views here.
